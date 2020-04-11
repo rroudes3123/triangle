d637fd08a3672052618901160af9284a56ca36c0
rroudes3123 <61859182+rroudes3123@users.noreply.github.com>
2020-04-11 03:31:18 -0600

    Add files via upload

--- shapes/triangle.py
+++ shapes/triangle.py
@@ -7,29 +7,32 @@ Purpose: Defines a Triangle object, inherited from the abstract class Shape.
 """
 
 from shapes.shape import Shape
-
+from math import sqrt
 
 class Triangle(Shape):
     """
     Represents a Triangle shape, and contains a length value
     and width value.
     """
+    decimal_places = 2
 
-    def __init__(self, length, width, height):
-        self.length = length
-        self.width = width
-        self.height = height
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
 
     def area(self):
         """
         Compute the area using the formula 1/2 * length * width
         """
-        return 1/2 * (self.height * self.width)
+        p = (self.a + self.b + self.c) / 2
+        q = p*(p-self.a)*(p-self.b)*(p-self.c)
+        sqrt(q)
+        return round(q, 2)
+
 
     def perimeter(self):
         """
         Compute the perimeter using the formula length + width + height
         """
-        return self.length + self.width + self.height
-
-
+        return self.a + self.b + self.c
